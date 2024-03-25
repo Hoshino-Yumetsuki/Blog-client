b7a520adf663890b279225d33b52c73a769aaad8
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2024-03-25 22:50:04 +0800

    .

--- themes/acryple/scripts/events/welcome.js
+++ themes/acryple/scripts/events/welcome.js
@@ -1,7 +1,5 @@
-const logger = require('hexo-log')()
-
 hexo.on('ready', () => {
   const { version } = require('../../package.json')
-  logger.info(`Hexo Acryple theme based Butterfly ${version}
+  hexo.log.info(`Hexo Acryple theme based Butterfly ${version}
 ============================================================`)
 })
